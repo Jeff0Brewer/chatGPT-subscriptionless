49065bced7be8506ac58149db61f03449f8a7c22
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-04-14 16:37:59 -0600

    init model dropdown

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,10 +1,33 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { MODELS, MODEL_INFO } from '@/lib/models'
 
 const Chat: FC = () => {
+    const [model, setModel] = useState<string>(MODELS[0])
+
     return (
         <main>
-            chat
+            <ModelDropdown model={model} setModel={setModel} />
         </main>
+    )
+}
+
+type ModelDropdownProps = {
+    model: string,
+    setModel: (model: string) => void
+}
+
+const ModelDropdown: FC<ModelDropdownProps> = props => {
+    return (
+        <div>
+            <button>{props.model}</button>
+            <div>{
+                MODELS.map((model: string) => (
+                    <a key={model}>
+                        {MODEL_INFO[model].name}
+                    </a>
+                ))
+            }</div>
+        </div>
     )
 }
 
