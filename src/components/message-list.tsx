22e707f672fc0b0c958bfa673a6a5d6ae83f42f4
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-05-06 21:10:29 -0600

    add and style edit button

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import ReactMarkdown from 'react-markdown'
 import Image from 'next/image'
+import { FiEdit } from 'react-icons/fi'
 import type { ChatCompletionRequestMessage as Message } from 'openai'
 import userIcon from '@/icons/user-icon.jpg'
 import gptIcon from '@/icons/gpt-icon.jpg'
@@ -30,7 +31,7 @@ type MessageDisplayProps = {
 }
 const MessageDisplay: FC<MessageDisplayProps> = props => {
     return (
-        <div className={styles.messageDisplay} data-role={props.role}>
+        <div className={styles.display} data-role={props.role}>
             <span className={styles.inner}>
                 <Image
                     className={styles.icon}
@@ -42,6 +43,7 @@ const MessageDisplay: FC<MessageDisplayProps> = props => {
                 <ReactMarkdown className={styles.content}>
                     {props.content}
                 </ReactMarkdown>
+                <button className={styles.edit}><FiEdit /></button>
             </span>
         </div>
     )
